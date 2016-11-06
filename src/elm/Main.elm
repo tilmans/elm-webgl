1069263d792cfd2a0f0066e0190e5120a2380c34
Tilman Schlenker <tilman.schlenker@avid.com>
2016-11-06 16:27:41 +0100

    quad separate

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,10 +1,9 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as Html
+import WebGL exposing (toHtml)
 
-import WebGL exposing (..)
-import Math.Vector3 exposing (..)
-import Math.Matrix4 exposing (..)
+import Quad exposing (..)
 
 model : number
 model = 0
@@ -27,50 +26,9 @@ update msg model =
 subscriptions model =
     Sub.none
 
-mesh1 =
-    TriangleStrip 
-        [ {position = (vec3 -1 0 0), color = (vec3 1 1 0)}
-        , {position = (vec3 0 0 0), color = (vec3 1 1 0)}
-        , {position = (vec3 0 1 0), color = (vec3 1 1 0)}
-        , {position = (vec3 -1 1 0), color = (vec3 1 1 0)}
-        , {position = (vec3 -1 0 0), color = (vec3 1 1 0)}
-        ]
-
-mesh2 =
-    TriangleStrip 
-        [ {position = (vec3 -0.95 0.95 0), color = (vec3 1 0 0)}
-        , {position = (vec3 -0.05 0.95 0), color = (vec3 1 0 0)}
-        , {position = (vec3 -0.05 0.05 0), color = (vec3 1 0 0)}
-        , {position = (vec3 -0.95 0.95 0), color = (vec3 1 0 0)}
-        , {position = (vec3 -0.95 0.05 0), color = (vec3 1 0 0)}
-        ]
-
 view model =
     WebGL.toHtml [width 400, height 400]
-    ( [ render vertexShader fragmentShader mesh1 {} ] ++
-        [ render vertexShader fragmentShader mesh2 {} ] )
+    (   (quad 1 (1, 0, 0)) 
+    ++  (quad 0.3 (1, 1, 0)) 
+    )
     
--- SHADERS
-vertexShader : Shader { attr | position:Vec3, color:Vec3 } {} { vcolor:Vec3}
-vertexShader = [glsl|
-    attribute vec3 position;
-    attribute vec3 color;
-    
-    varying vec3 vcolor;
-    
-    void main () {
-        gl_Position = vec4(position, 1.0);
-        vcolor = color;
-    }
-|]
-
-fragmentShader : Shader {} {} { vcolor:Vec3 }
-fragmentShader = [glsl|
-    precision mediump float;
-    
-    varying vec3 vcolor;
-    
-    void main () {
-        gl_FragColor = vec4(vcolor,1);
-    }
-|]

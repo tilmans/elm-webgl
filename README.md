c3f9960f3ad7a2b873c670f5890e0e58acb75c58
Tilman Schlenker <tilman.schlenker@avid.com>
2016-11-06 18:30:36 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Simple Elm WebGL/Webpack project
+Examples of Elm WebGL apps based on examples from "Generative Gestaltung". You can [view the examples online on the project page](https://tilmans.github.io/elm-webgl/).
+
 Based on elm-webpack-starter
 
     npm install
--- src/static/index.js
+++ src/static/index.js
@@ -4,8 +4,15 @@ require( './styles/main.scss' );
 var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if Bootstrap's JS not needed
 require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' ); // <--- remove if Bootstrap's JS not needed 
 
+$("#root").click(function() {
+    console.log("Root");
+    $("#main").html("");
+    $("#text").show();
+});
+
 $("#1_0").click(function() {
     // inject bundled Elm app into div#main
+    $("#main").html("");
     var Elm = require( '../elm/1.0/Main' );
     Elm.Main.embed( document.getElementById( 'main' ) );    
     $("#text").hide();

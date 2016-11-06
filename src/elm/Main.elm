fd764cbd2c48ddcf4687cef0f74571a98fe77e2d
Tilman Schlenker <tilman.schlenker@avid.com>
2016-11-06 17:25:16 +0100

    done

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,33 +2,61 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as Html
 import WebGL exposing (toHtml)
+import Mouse
+import Color exposing (..)
 
 import Quad exposing (..)
 
-model : number
-model = 0
+type alias Model =
+    { width: Float
+    , colorBack: (Float, Float, Float)
+    , colorFront: (Float, Float, Float)
+    }
 
-type Msg = NoOp
+type Msg = MouseMsg Mouse.Position
 
 main : Program Never
 main =
   Html.program 
-    { init = (0, Cmd.none)
+    { init = ((Model 0 (1,0,0) (0,1,0)), Cmd.none)
     , view = view
     , subscriptions = subscriptions
     , update = update 
     }
 
+widthFor x =
+    Basics.min 1.0 (x/400)
+    
+colorsFor y = 
+    let
+        offset = Basics.min 1.0 (y/400)
+        radian = (degrees 360*offset)
+        rgbF = toRgb (hsl radian 1 0.5)
+        rgbB = toRgb (hsl (radian * -1) 1 0.5)
+    in
+        ( ((toFloat rgbF.red)/255, (toFloat rgbF.green)/255, (toFloat rgbF.blue)/255)
+        , ((toFloat rgbB.red)/255, (toFloat rgbB.green)/255, (toFloat rgbB.blue)/255)
+        )
+
 update msg model =
     case msg of
-        NoOp -> (model, Cmd.none)
+        MouseMsg position -> 
+            let
+                width = widthFor (toFloat position.x)
+                (colorFront, colorBack) = colorsFor (toFloat position.y)
+            in
+            ({model|
+                width=width,
+                colorFront=colorFront,
+                colorBack=colorBack
+            }, Cmd.none)
         
 subscriptions model =
-    Sub.none
+    Mouse.moves MouseMsg
 
 view model =
     WebGL.toHtml [width 400, height 400]
-    (   (quad 1 (1, 0, 0)) 
-    ++  (quad 0.3 (1, 1, 0)) 
-    )
+        (   (quad 1 model.colorBack) 
+        ++  (quad model.width model.colorFront) 
+        )
     
